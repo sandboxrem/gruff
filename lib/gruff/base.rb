839988a40da3e2ae2b98bd0e555c9b5af62291f4
Mikko Vulli <mikkovulli@gmail.com>
2012-02-02 16:07:33 +0200

    Remove two unnecessary assignments

--- lib/gruff/base.rb
+++ lib/gruff/base.rb
@@ -1092,7 +1092,7 @@ module Gruff
     # handle.
     def calculate_caps_height(font_size)
       @d.pointsize = font_size
-      @d.font = @font
+      #@d.font = @font
       @d.get_type_metrics(@base_image, 'X').height
     end
 
@@ -1102,7 +1102,7 @@ module Gruff
     # scaling will handle.
     def calculate_width(font_size, text)
       @d.pointsize = font_size
-      @d.font = font
+      #@d.font = font
       @d.get_type_metrics(@base_image, text.to_s).width
     end
 
